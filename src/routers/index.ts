c521aa615a2430fe660831093f2f5e037414544d
Apple <apple@Apples-MacBook-Pro.local>
2023-04-20 13:52:00 +0530

    add userdetails api for mother

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -8,10 +8,12 @@ import userRouter from "../components/Users/UserRouter";
 import authRouter from "../components/Auth/AuthRouter";
 import clinicRouter from '../components/Clinic/ClinicRouter';
 import appointmentRouter from '../components/Appointment/AppointmentRouter';
+import userDetailsRouter from '../components/UserDetails/UserDetailsRouter'
 const router = Router();
 
 router.use("/v1/users", userRouter);
-router.use('/v1/auth',authRouter)
-router.use('/v1/clinic',clinicRouter)
-router.use('/v1/appointment',appointmentRouter)
+router.use('/v1/auth', authRouter)
+router.use('/v1/clinic', clinicRouter)
+router.use('/v1/appointment', appointmentRouter)
+router.use('/v1/mother', userDetailsRouter)
 export default router;
