79c1c15f1ed671cde753e97639a90cad49ef326a
Apple <apple@Apples-MacBook-Pro.local>
2023-05-05 16:00:10 +0530

    changes of this week

--- dist/CMS/CMSRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const CMSController_1 = require("./CMSController");
-const router = (0, express_1.Router)();
-router.post("/weekly/content/create", CMSController_1.createWeeklyContent);
-router.put("/weekly/content/update", CMSController_1.updateWeeklyContent);
-router.get("/weekly/content/details", CMSController_1.getWeeklyContent);
-exports.default = router;
