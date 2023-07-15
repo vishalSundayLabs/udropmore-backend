43daa09cb7d6328821f65bae1d534c8685c30ffd
Apple <apple@192.168.1.4>
2023-07-15 16:25:29 +0530

    changes of 15/07/23

--- dist/utils/CreateOtp.js
+++ dist/utils/CreateOtp.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createOtp = void 0;
+const Config_1 = require("../config/Config");
 const createOtp = () => {
-    return process.env.DEV_OTP ? process.env.DEV_OTP : Math.floor(Math.random() * 1000) + 1000;
+    return Config_1.default.DEV_OTP ? Config_1.default.DEV_OTP : Math.floor(Math.random() * 1000) + 1000;
 };
 exports.createOtp = createOtp;
--- src/utils/CreateOtp.ts
+++ src/utils/CreateOtp.ts
@@ -1,3 +1,5 @@
+import config from "../config/Config";
+
 export const createOtp = () => {
-    return process.env.DEV_OTP ? process.env.DEV_OTP : Math.floor(Math.random() * 1000) + 1000
+    return config.DEV_OTP ? config.DEV_OTP : Math.floor(Math.random() * 1000) + 1000
 }
