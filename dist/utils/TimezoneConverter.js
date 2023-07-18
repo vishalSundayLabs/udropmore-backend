daa40f5d4eef954b9920f77a58192943e788500d
Apple <apple@Apples-MacBook-Pro.local>
2023-07-18 20:39:35 +0530

    changes of 18/07/23

--- dist/utils/TimezoneConverter.js
+++ dist/utils/TimezoneConverter.js
@@ -1,13 +1,19 @@
 "use strict";
+// export const getISTmsTime = (time = null) => {
+//     var timeZone = 'Asia/Kolkata';
+//     console.log(time)
+//     var ISToffSet = 330; //IST is 5:30; i.e. 60*5+30 = 330 in minutes 
+//     let offset = ISToffSet * 60 * 1000;
+//     var currentDate = time ? new Date(time) : new Date().toLocaleString('en-US', { timeZone: timeZone });
+//     var milisecond = time ? new Date(currentDate).getTime() - offset : new Date(currentDate).getTime() + offset;
+//     return milisecond
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getISTmsTime = void 0;
 const getISTmsTime = (time = null) => {
     var timeZone = 'Asia/Kolkata';
-    console.log(time);
-    var ISToffSet = 330; //IST is 5:30; i.e. 60*5+30 = 330 in minutes 
-    let offset = ISToffSet * 60 * 1000;
-    var currentDate = time ? new Date(time) : new Date().toLocaleString('en-US', { timeZone: timeZone });
-    var milisecond = time ? new Date(currentDate).getTime() - offset : new Date(currentDate).getTime() + offset;
+    var currentDate = time ? new Date(time).toLocaleString('en-US', { timeZone: timeZone }) : new Date().toLocaleString('en-US', { timeZone: timeZone });
+    var milisecond = new Date(currentDate).getTime();
     return milisecond;
 };
 exports.getISTmsTime = getISTmsTime;
--- src/utils/TimezoneConverter.ts
+++ src/utils/TimezoneConverter.ts
@@ -1,9 +1,16 @@
+// export const getISTmsTime = (time = null) => {
+//     var timeZone = 'Asia/Kolkata';
+//     console.log(time)
+//     var ISToffSet = 330; //IST is 5:30; i.e. 60*5+30 = 330 in minutes 
+//     let offset = ISToffSet * 60 * 1000;
+//     var currentDate = time ? new Date(time) : new Date().toLocaleString('en-US', { timeZone: timeZone });
+//     var milisecond = time ? new Date(currentDate).getTime() - offset : new Date(currentDate).getTime() + offset;
+//     return milisecond
+// }
+
 export const getISTmsTime = (time = null) => {
     var timeZone = 'Asia/Kolkata';
-    console.log(time)
-    var ISToffSet = 330; //IST is 5:30; i.e. 60*5+30 = 330 in minutes 
-    let offset = ISToffSet * 60 * 1000;
-    var currentDate = time ? new Date(time) : new Date().toLocaleString('en-US', { timeZone: timeZone });
-    var milisecond = time ? new Date(currentDate).getTime() - offset : new Date(currentDate).getTime() + offset;
+    var currentDate = time ? new Date(time).toLocaleString('en-US', { timeZone: timeZone }) : new Date().toLocaleString('en-US', { timeZone: timeZone });
+    var milisecond = new Date(currentDate).getTime()
     return milisecond
 }
